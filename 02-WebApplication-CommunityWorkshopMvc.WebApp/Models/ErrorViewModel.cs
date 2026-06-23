109668b3f23b363f4bdf1fa522ffbaa9d6f58d01
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-06-23 12:06:21 -0300

    chore: initialize parallel MVC project structure

---
+++ 02-WebApplication-CommunityWorkshopMvc.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _02_WebApplication_CommunityWorkshopMvc.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
